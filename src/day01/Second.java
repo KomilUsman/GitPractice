44c1df44bc65cc9e7ad4c12bbe72ef95d1981047
Komil <87507641+KomilUsman@users.noreply.github.com>
2021-07-26 11:25:57 -0400

    added new file

---
+++ src/day01/Second.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class Second {
+    public static void main(String[] args) {
+        for (int i = 1; i <=6 ; i++) {
+            System.out.println("Hello world "+i+" times");
+        }
+    }
+}
